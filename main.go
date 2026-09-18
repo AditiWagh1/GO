ca8237b08368bef195fa46ff25e3e38bda55ed89
AditiWagh1 <mikadu.wagh@gmail.com>
2026-09-18 19:02:08 +0530

    new file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("hello world")
+	fmt.Println(1+1)
+	fmt.Println(true)
+	var name="aditi"
+	//shortcut
+	name1 :="golang"
+	fmt.Println(name)
+	fmt.Println(name1)
+
+	//if u have to assign value later
+	var name2 string
+	name2="golang"
+	fmt.Println(name2)
+
+}
