46fb821385b018eabae916ebdfb3bc84ecb04cd8
AditiWagh1 <mikadu.wagh@gmail.com>
2026-09-18 20:23:02 +0530

    new file

---
+++ if_else.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// age := 18
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult")
+	// } else if age>=12{
+	// 	fmt.Println("Person is  a teenager")
+	// }  else{
+	// 	fmt.Println("Person is not an adult")
+	// }
+
+	//directly declaring in if statement
+	if age:=15; age>=18{
+		fmt.Println("person is an adult", age)
+	} else if age>=12{
+		fmt.Println("person is teenager",age)
+	}
+	//go does not have ternary operator
+}
