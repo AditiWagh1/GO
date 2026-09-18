e505b5d6f988f47f03eb278f65b966ee0ae8276f
AditiWagh1 <mikadu.wagh@gmail.com>
2026-09-18 20:24:14 +0530

    new file

---
+++ switch.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	//"time"
+)
+
+func main() {
+	// simple switch
+	// i := 5
+
+	// switch i {
+	// case 1:
+	// 	fmt.Println("one")
+	// case 2:
+	// 	fmt.Println("two")
+	// case 3:
+	// 	fmt.Println("three")
+	// default:
+	// 	fmt.Println("other")
+	// }
+
+	//multiple condition switch
+	// switch time.Now().Weekday(){
+	// case time.Saturday,time.Sunday:
+	// 	fmt.Println("It's weekend")
+	// default:
+	// 	fmt.Println("It's workday")
+	// }
+
+	//type swich
+	whoAmI:= func(i interface{}) {
+		switch i :=i.(type){
+		case int:
+			fmt.Println("its an integer")
+		case string:
+			fmt.Println("its a string")
+		case bool:
+			fmt.Println("its a boolean")
+		default:
+			fmt.Println("other",i)
+		}
+	}
+	whoAmI(65.76)
+}
